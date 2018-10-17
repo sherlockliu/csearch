fd02307a94ec36042a79b91be80456a77fcaaee8
unknown <568916617@qq.com>
2018-10-17 23:34:12 +0800

    change setup

--- scripts/setup_for_dev.sh
+++ scripts/setup_for_dev.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
 
-command -v pyenv >/dev/null 2>&1 || { echo "Installing pyenv" >&2; brew install pyenv; }
-
-echo "-> Installing python from .python-version with pyenv <-"
-pyenv install
-
-eval "$(pyenv init -)"
-
 pip install virtualenv
 
 echo "-> Creating virtualenv..."
-virtualenv --setuptools --no-site-packages --prompt="(${PWD##*/}) " -p python3 .venv
+virtualenv --setuptools --no-site-packages --prompt="(${PWD##*/}) " -p python .venv
 
 echo "-> Activating virtualenv..."
-source .venv/bin/activate
+source .venv/scripts/activate
 
 echo "Upgrading and installing pip dependencies..."
-pip install --upgrade pip==9.0.3
 pip install --upgrade setuptools
-pip install --upgrade --requirement requirements/dev-requirements.txt
+pip install --upgrade --requirement requirements/requirement.txt
 
 deactivate
 echo "-> Pip dependencies install complete!"
-
-echo "Updating /etc/hosts to include docker.local and megatron-nginx"
-source ./update_hosts.sh
 
 echo "Done!"
