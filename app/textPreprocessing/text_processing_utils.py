fad70da83918c450c3068b533a948ff0f290bb80
zhulinhua <493295396@qq.com>
2018-10-25 17:12:14 +0800

    编辑距离纠错函数

--- app/textPreprocessing/text_processing_utils.py
+++ app/textPreprocessing/text_processing_utils.py
@@ -64,6 +64,28 @@ def get_idmap(hotelids):
 word2Vec = KeyedVectors.load_word2vec_format(cfg.get('model','word2vec_path'))
 seg = Seg()
 
+
+def levenshtein(s, t):
+    if s == t:
+        return 0
+    elif len(s) == 0:
+        return len(t)
+    elif len(t) == 0:
+        return len(s)
+    v0 = [None] * (len(t) + 1)
+    v1 = [None] * (len(t) + 1)
+    for i in range(len(v0)):
+        v0[i] = i
+    for i in range(len(s)):
+        v1[0] = i + 1
+        for j in range(len(t)):
+            cost = 0 if s[i] == t[j] else 1
+            v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+        for j in range(len(v0)):
+            v0[j] = v1[j]
+
+    return v1[len(t)]
+
 def get_similar_words(sentence,num):
     result_list = []
     sililar_list = []
@@ -100,6 +122,14 @@ def get_pinyin_words(words):
         return ""
     else:
         return result
+def get_least_levenshtein(word,minDistance = 1):
+    
+    for highFrequency in cfg.get_highFrequencySearch():
+        tmpDistance = levenshtein(highFrequency,word)
+        if tmpDistance <= 1:
+            return highFrequency
+    return ''
+
 
 
 
